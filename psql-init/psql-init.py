f35f60eb2805797d126b393f5c1577d48c28dd3e
Blair Drummond <blair.drummond@canada.ca>
2021-05-23 11:32:05 -0400

    fix: use python3 explicitly & change mount

--- psql-init/psql-init.py
+++ psql-init/psql-init.py
@@ -173,7 +173,7 @@ except(Exception, psycopg2.DatabaseError) as error:
 # replace ckan.plugins so that ckan cli can run and apply datastore permissions
 sed_string = "s/ckan.plugins =.*/ckan.plugins = envvars image_view text_view recline_view datastore/g" # noqa
 subprocess.Popen(["/bin/sed", sed_string, "-i", "/srv/app/production.ini"])
-sql = subprocess.check_output(["/usr/bin/ckan",
+sql = subprocess.check_output(["ckan",
                                "-c", "/srv/app/production.ini",
                                "datastore",
                                "set-permissions"],
